2623c5abe861032afb9e2de6b7f73e455f175abe
Oswald786 <ethanslade63@gmail.com>
2024-08-26 16:16:11 +0100

    FINISHED AND COMPLETED CODE
    ITS ALL FINSIHED AND SORTED WIHT THE UPDATED CODE

--- mainWebpage/javascript/JavascriptMain.js
+++ mainWebpage/javascript/JavascriptMain.js
@@ -1,12 +1,36 @@
-function OpenFilters(){
-    if (filterSelections.style.display !== "none"){
-        filterSelections.style.display = "none"
+function openFilters(){
+    var x = document.getElementById("hiddenElement")
+    if (x.style.display !== "none"){
+        x.style.display = "none"
     }
     else {
-        filterSelections.style.display = "flex"
+        x.style.display = "flex"
     }
 
 }
 
-filterSelections = document.getElementById("filterSelections").innerHTML
-filterSelections = false
+var JuniorIsa = document.getElementById("Junior isa")
+var regularSaver = document.getElementById("First Home Saver (2)")
+var FourAccessIsa = document.getElementById("FourAccessIsa")
+var FourAccessSaver = document.getElementById("Four Access Saver (2)")
+var FixedBond = document.getElementById("Fixed Bond (344) 31.08.2026")
+var EasyAccessSaver = document.getElementById("Easy Access Saver (7)")
+
+AccountKinds = [JuniorIsa,regularSaver,FourAccessIsa,FourAccessSaver,FixedBond]
+
+VariableAccounts = [JuniorIsa,regularSaver,FourAccessIsa,FourAccessSaver]
+
+
+function ShowVariable(){
+    showing = false
+    for (var i =0; i < AccountKinds.length ; i++){
+        if (VariableAccounts.includes(AccountKinds[i])){
+            console.log("accounts found")
+            
+        }
+        else{
+            AccountKinds[i].style.display = "none"
+        }
+    
+
+}}
